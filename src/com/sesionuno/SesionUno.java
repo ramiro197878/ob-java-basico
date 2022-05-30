ad16822d42aba6b2a077a4f34147c59fed92666b
Ramiro Astete <24412705+ramiro197878@users.noreply.github.com>
2022-05-30 22:12:37 +0200

    Session uno terminada

---
+++ src/com/sesionuno/SesionUno.java
@@ -0,0 +1,35 @@
+package com.sesionuno;
+
+public class SesionUno {
+
+    public static void main(String[] args) {
+
+        byte varByte = 127;
+        System.out.println(varByte);
+
+        short varShort = 22;
+        System.out.println(varShort);
+
+        int varInt = 123;
+        System.out.println(varInt);
+
+        long varLong = 20033232;
+        System.out.println(varLong);
+
+        float varFloat = 43.2f;
+        System.out.println(varFloat);
+
+        double varDouble = 321.33d;
+        System.out.println(varDouble);
+
+        boolean varBoolean = true;
+        System.out.println(varBoolean);
+
+        char varChar = 'z';
+        System.out.println(varChar);
+
+        String varString = "String en la primera sesión";
+        System.out.println(varString);
+
+    }
+}
