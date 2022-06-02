cb1a46878badb501f3d8fa5438009cae8ef22704
Ramiro Astete <24412705+ramiro197878@users.noreply.github.com>
2022-06-02 17:30:17 +0200

    modificacion .gitignore

---
+++ src/com/sesioncuatro/Main.java
@@ -0,0 +1,16 @@
+package com.sesioncuatro;
+
+//En este ejercicio tendréis que crear una clase SmartDevice. Dentro crearéis las clases hijas: SmartPhone y SmartWatch.
+//
+//Agregaréis atributos tal cual tendrían esos objetos en la realidad.
+//
+//Crear constructor vacío y con todos los parámetros para cada clase.
+//
+//Desde una clase Main: crearéis objetos de cada una y los utilizaréis para imprimir sus valores por consola.
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/sesioncuatro/SmartPhone.java
@@ -0,0 +1,4 @@
+package com.sesioncuatro;
+
+public class SmartPhone {
+}
